1945e8fbfc7c39c7d3ea93964c1b8731558fa474
P1P3HD <pipehuenchuleo123@gmail.com>
2025-04-08 18:15:52 -0400

    dea

--- bucles/bucle_for.py
+++ bucles/bucle_for.py
@@ -5,6 +5,25 @@ numeros = [10,20,30,40]
 for juego in juegos: 
     print(juego) 
 
+print()
 for numero in numeros:
     resultado = numero * numero
     print(f"El resultado de multiplicar {numero} * {numero} = {resultado}")
+
+print()
+for num in range(5):
+    print(num)
+
+print()
+for num in range(5,16):
+    print(num)
+
+print()
+for num in range(5,16,5):
+    print(num)
+
+for elemento in enumerate(numeros):
+    indice = elemento[0]
+    valor = elemento[1]
+    print(f"El índice es: {indice} y el valor es: {valor}")
+
