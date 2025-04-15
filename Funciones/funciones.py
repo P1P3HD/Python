2ba47610251f77e27c93fdb11078cbecf939099a
P1P3HD <pipehuenchuleo123@gmail.com>
2025-04-15 17:26:26 -0400

    CAQUITA

--- Funciones/funciones.py
+++ Funciones/funciones.py
@@ -1,5 +1,43 @@
+# Ejercicio 1
+# Definir función saludar 
 def saludar(nombre):
     print(f"Buen día estimado(a) {nombre}")
 
 nombre = input("Ingresa su nombre: ")
+# Ejecutar función saludar
 saludar(nombre)
+
+# Ejercicio 2
+# Definir función sumar
+def suma(a,b):
+    resultado = a + b
+    print(f"El resultado de sumar {a} + {b} = {resultado}")
+
+numero_1 = int(input("Ingrese su primer número:"))
+numero_2 = int(input("Ingrese su segundo número:"))
+# Ejecutar función suma
+suma(numero_1,numero_2)
+
+# Ejercicio 3
+
+num_1 = int(input("Ingrese su primer número:"))
+num_2 = int(input("Ingrese su segundo número:"))
+operacion =  int(input("Ingrese el tipo de operación que necesita:"))
+
+# Definir función calculadora
+def calculadora(a,b,op):
+    resultado = 0
+
+    if op == "+":
+        resultado = a + b
+    elif op == "*":
+        resultado = a * b
+    elif op == "-":
+        resultado = a - b
+    else:
+        resultado = a / b
+    print(f"El resultado de {a}{op}{b} = {resultado}")
+
+# Ejecución función calculadora 
+calculadora(num_1, num_2, operacion)
+
