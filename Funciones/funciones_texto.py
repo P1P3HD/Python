4fe14d761c5e934c0b5d49268bdbde8afe76ca00
Pipe <pipehuenchuleo123@gmail.com>
2025-04-17 18:23:27 -0400

    QUEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- Funciones/funciones_texto.py
+++ Funciones/funciones_texto.py
@@ -1,5 +1,5 @@
-nombre = "Adolf"
-apellido = "Hitler"
+nombre = "adolf"
+apellido = "hitler"
 
 nombre_mayusculas = nombre.upper()
 apellido_mayusculas = apellido.upper()
@@ -7,6 +7,16 @@ apellido_mayusculas = apellido.upper()
 nombre_minusculas = nombre.lower()
 apellido_minusculas = apellido.lower()
 
+nombre_titulo = nombre.title()
+apellido_titulo = apellido.title()
+
+print("Adolf".casefold() in nombre)
+print(nombre_mayusculas.count("A"))
+print(nombre_mayusculas.encode("utf-8"))
+print(nombre.endswith("f"))
+
 print(f"Hola admirable y maravilloso {nombre} {apellido} {34+35}")
+print(f"Hola admirable y maravilloso {nombre} {apellido} {34+35}".upper())
 print(f"Nombre y apellido en mayúsculas: {nombre_mayusculas} {apellido_mayusculas}")
 print(f"Nombre y apellido en minúsculas: {nombre_minusculas} {apellido_minusculas}")
+print(f"Nombre y apellido en título: {nombre_titulo} {apellido_titulo}")
