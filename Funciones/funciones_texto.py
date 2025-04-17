46173266337aa08c15db53d85ce21314604daadc
Pipe <pipehuenchuleo123@gmail.com>
2025-04-17 18:08:05 -0400

    DSADSAD

---
+++ Funciones/funciones_texto.py
@@ -0,0 +1,12 @@
+nombre = "Adolf"
+apellido = "Hitler"
+
+nombre_mayusculas = nombre.upper()
+apellido_mayusculas = apellido.upper()
+
+nombre_minusculas = nombre.lower()
+apellido_minusculas = apellido.lower()
+
+print(f"Hola admirable y maravilloso {nombre} {apellido} {34+35}")
+print(f"Nombre y apellido en mayúsculas: {nombre_mayusculas} {apellido_mayusculas}")
+print(f"Nombre y apellido en minúsculas: {nombre_minusculas} {apellido_minusculas}")
