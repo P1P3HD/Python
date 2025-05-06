3c828b2bae636863674847fedc0e5a6913e4de26
Pipe <pipehuenchuleo123@gmail.com>
2025-05-06 18:27:19 -0400

    ASD

---
+++ Modulos/funciones/principal.py
@@ -0,0 +1,9 @@
+import cuadirlatero
+
+print(cuadirlatero.perimetro(2,3))
+print(cuadirlatero.area(2,3))
+print(cuadirlatero.volumen(2,3,4))
+
+def menu_principal():
+    print()
+    print("1: Perímetro")
