85229b5dfe04d8ea0ff40930bae5046495856ef1
Pipe <pipehuenchuleo123@gmail.com>
2025-06-19 18:28:31 -0400

    ds

---
+++ manejo_notas/data/gestion_notas_DDL.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE gestion_notas;
+
+USE gestion_notas;
+
+CREATE TABLE IF NOT EXISTS docentes(
+    id INT NOT NULL AUTO_INCREMENT,
+    rut VARCHAR(12) NOT NULL UNIQUE,
+    nombre VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NULL,
+
+    CONSTRAINT pk_docente PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS asignaturas(
+    id INT NOT NULL AUTO_INCREMENT,
+    codigo VARCHAR(12) NOT NULL,
+    nombre VARCHAR(250) NOT NULL,
+    descripcion VARCHAR(250) NULL,
+
+    CONSTRAINT pk_asignaturas PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS docentes_asignaturas(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_docente INT NOT NULL,
+    id_asignatura INT NOT NULL,
+
+    CONSTRAINT pk_docentes_asignaturas PRIMARY KEY (id),
+    CONSTRAINT fk_docentes FOREIGN KEY (id_docente) REFERENCES docentes(id),
+    CONSTRAINT fk_asignaturas FOREIGN KEY (id_asignatura) REFERENCES asignaturas(id)
+);
+
+ALTER TABLE 'docentes' ADD UNIQUE('rut');
+ALTER TABLE 'asignaturas' ADD UNIQUE('codigo');
+ALTER TABLE 'docentes' ADD habilitado TINYINT NOT NULL DEFAULT 1;
