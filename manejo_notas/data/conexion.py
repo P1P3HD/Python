36a16318a21770a4c3f20ec84cc9838d76c9631c
Pipe <pipehuenchuleo123@gmail.com>
2025-06-24 18:28:19 -0400

    tgdjfsagndkmsfhnsdgmoihlektmshetrs

--- manejo_notas/data/conexion.py
+++ manejo_notas/data/conexion.py
@@ -1,5 +1,7 @@
 import mysql.connector
 
+# CTRL + SHIFT + P para cambiar el interpete de python, y ejecutar "pip3 install mysql-connector"
+
 conexion = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
@@ -9,8 +11,7 @@ conexion = mysql.connector.connect(
 
 cursor = conexion.cursor()
 
-cursor.execute('SELECT * FROM docentes')
-resultado = cursor.fetchall()
-print(resultado) 
-
-# CTRL + SHIFT + P para cambiar el interpete de python, y ejecutar "pip3 install mysql-connector"
+def ejecutar_consulta(consulta):
+    cursor.execute('SELECT * FROM asignaturas')
+    resultado = cursor.fetchall()
+    return resultado
