0c184135f61106e8914b22256e1340e5700851c3
Pipe <pipehuenchuleo123@gmail.com>
2025-07-03 18:29:58 -0400

    s

---
+++ manejo_dotas/data/conexion.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+# CTRL + SHIFT + P para cambiar el interpete de python, y ejecutar "pip3 install mysql-connector"
+# INSTALAR PRETTYTABLE
+
+conexion = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    user='root',
+    database='gestion_notas'
+)
+
+cursor = conexion.cursor()
+
+def ejecutar_consulta(consulta):
+    cursor.execute('SELECT * FROM asignaturas')
+    resultado = cursor.fetchall()
+    return resultado
---
+++ manejo_dotas/principal.py
@@ -0,0 +1,44 @@
+from data.asignaturas import lista_asignaturas
+import os
+
+print(len(lista_asignaturas))
+print(len("Hola"))
+
+def mostrar_listado_asignatura():
+    print()
+    print('Lista de Asignaturas')
+    print('====================')
+    contador = 0
+    for asignatura in sorted(lista_asignaturas):
+        contador += 1
+        print(f'{contador}- {asignatura}')
+
+def buscar_asignatura():
+    busqueda = input('Ingrese asignatura a buscar: ')
+    for asignatura in lista_asignaturas:
+        if busqueda.lower() in asignatura.lower():
+            return asignatura
+
+def agregar_asignatura():
+    mostrar_listado_asignatura()
+    nueva_asignatura = input('Ingrese nueva asignatura: ')
+    lista_asignaturas.append(nueva_asignatura.title())
+    nombre_archivo = 'asignaturas.py'
+    ruta_relativa = os.path.join('manejo_notas/data', nombre_archivo)
+    ruta_absoluta = os.path.abspath(ruta_relativa)
+    ruta_real = os.path.realpath(ruta_absoluta)
+    archivo_final = open(ruta_real,'w+')
+    archivo_final.write(f'asignaturas={lista_asignaturas}')
+    archivo_final.close()
+    mostrar_listado_asignatura()
+
+def actualizar_asignatura():
+    mostrar_listado_asignatura()
+    busqueda = input('Ingresa asignatura a buscar: ')
+    for i in range(len(lista_asignaturas)):
+        if busqueda.lower() in lista_asignaturas[i].lower():
+            nuevo_dato = input(f'Ingrese nuevo nombre para asignatura {lista_asignaturas[i]}: ')
+            lista_asignaturas[i] = nuevo_dato
+    mostrar_listado_asignatura()
+
+agregar_asignatura()
